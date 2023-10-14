53d737c97d9b375a451de755a082041d561a46cc
Rohan Kishore <109947257+rohankishore@users.noreply.github.com>
2023-10-14 07:38:45 +0530

    #2 Added build.py

---
+++ build.py
@@ -0,0 +1,27 @@
+import subprocess
+import sys
+import os
+
+def run_pyinstaller():
+    try:
+        # Replace 'your_script.py' with the name of your main Python script
+        main_script = 'main.py'
+
+        # PyInstaller command to build the executable
+        cmd = [
+            'pyinstaller',
+            main_script,
+            '--onefile',  # Create a single executable file
+            '-w', # Makes it windowed
+            '--icon="icon.ico"'
+        ]
+
+        # Run PyInstaller
+        subprocess.check_call(cmd)
+
+        print("Build successful.")
+    except Exception as e:
+        print(f"Build failed: {e}")
+
+if __name__ == '__main__':
+    run_pyinstaller()
