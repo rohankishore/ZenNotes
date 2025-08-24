b073b2431fdf03c4328a556761714a492ca79152
Matthew Yang <matthew.yang204@gmail.com>
2025-08-24 16:49:13 -0700

    Start creating linux installer

--- install.py
+++ install.py
@@ -4,9 +4,24 @@ import sys
 class platformError(Exception):
     pass
 
+class linux:
+    def install():
+        import os
+
+        if os.path.isdir("dist"):
+            print("Found existing build, proceeding to install it...")
+        else:
+            print("ERROR: No build found")
+            print("Building first...")
+            import build
+            build.run_pyinstaller()
+            print("Build complete, installing build...")
+        
+        
+
 if platform.system() == "Windows":
     raise platformError("We have detected that you are using Windows. Please use the Inno Setup script instead.")
 elif platform.system() == "Linux":
-    import installers.linux
+    linux.install()
 else:
     raise platformError("Unsupported platform detected. There is no installer for this platform.")
