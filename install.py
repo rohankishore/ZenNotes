c8557eebc4efd742677689e49be33d1d7118d39c
Matthew Yang <matthew.yang204@gmail.com>
2025-08-24 17:17:30 -0700

    Add the rest of the linux installer

--- install.py
+++ install.py
@@ -17,7 +17,34 @@ class linux:
             build.run_pyinstaller()
             print("Build complete, installing build...")
         
+        installdirDefault = "/opt/rohankishore/zennotes"
+        installdir = input(f"Select installation directory (default {installdirDefault}, just press enter):")
+        if not installdir:
+            installdir = installdirDefault
+        else:
+            installdir = os.path.expanduser(installdir)
+        username = os.getenv("USER")
+        if username == "root":
+            lnDir = "/usr/local/bin"
+            deskDir = "/usr/share/applications"
+            os.makedirs(lnDir, exist_ok=True)
+            os.makedirs(deskDir, exist_ok=True)
+        else:
+            lnDir = f"/home/{username}/.local/bin"
+            deskDir = f"/home/{username}/.local/share/applications"
+            os.makedirs(lnDir, exist_ok=True)
+            os.makedirs(deskDir, exist_ok=True)
+
+        print(f"Installing to {installdir}...")
+        os.makedirs(installdir, exist_ok=True)
+        os.system(f"cp -r dist/main/* {installdir}/")
         
+        print("Creating symlinks...")
+        os.system(f"ln -sf {installdir}/main {lnDir}/zennotes")
+
+        print("Creating desktop shortcuts...")
+        os.system(f"cp zennotes.desktop {deskDir}/zennotes.desktop")
+        os.system(f"sed -i 's#@INSTALLDIR@#{installdir}#g' {deskDir}/zennotes.desktop")
 
 if platform.system() == "Windows":
     raise platformError("We have detected that you are using Windows. Please use the Inno Setup script instead.")
