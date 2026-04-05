840924253b917e62c2d21966f19afa76e2efbc5b
Matthew Yang (杨佳明) <matthew.yang204@gmail.com>
2026-04-05 16:51:13 -0400

    Import encodings & some file i/o functions from notepad==

---
+++ src/zencodings.py
@@ -0,0 +1,5 @@
+# Just a wrapper file for Notepad== contents
+# Import encodings, defines file i/o with multiple encodings, etc
+
+from notepadequalequal.common import encodings
+from notepadequalequal.fileio import retrieve_file
