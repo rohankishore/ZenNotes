472b02ee3ec14d349041ef76b4e2edd19b5d937e
Matthew Yang (杨佳明) <matthew.yang204@gmail.com>
2026-04-05 19:21:39 -0400

    Add notepadequal module to module import pathspace

--- src/zencodings.py
+++ src/zencodings.py
@@ -1,5 +1,9 @@
 # Just a wrapper file for Notepad== contents
 # Import encodings, defines file i/o with multiple encodings, etc
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'notepadequalequal'))
 
 from notepadequalequal.common import encodings
 from notepadequalequal.fileio import retrieve_file
