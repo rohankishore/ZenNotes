260f20191c3e52995943109bdbf8d55ff67e3c4b
Matthew Yang (杨佳明) <matthew.yang204@gmail.com>
2026-05-12 17:05:55 -0700

    Now we don't need this anymore due to the module usage of Notepad== fixed

--- src/zencodings.py
+++ src/zencodings.py
@@ -3,8 +3,6 @@
 import os
 import sys
 
-sys.path.append(os.path.join(os.path.dirname(__file__), 'notepadequalequal'))
-
 from notepadequalequal.common import encodings
 from notepadequalequal.fileio import retrieve_file
 
